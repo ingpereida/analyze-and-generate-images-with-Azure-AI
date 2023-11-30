8d756fb55120d14716975e664e7a512f32e9df6d
Cesar Geovanni Machuca Pereida <cesar.geovanni@gmail.com>
2023-11-30 00:44:34 -0700

    Commit made from VS Code Azure Static Web Apps

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [imageUrl, setImageUrl] = useState('');
+
+  const handleInputChange = (event) => {
+    setImageUrl(event.target.value);
+  };
+
+  const handleAnalyzeButtonClick = () => {
+    // Aquí va el código para analizar la imagen
+  };
+
+  const handleGenerateButtonClick = () => {
+    // Aquí va el código para generar la imagen
+  };
+
+  return (
+    <div className="App">
+      <h1>Computer Vision</h1>
+      <input type="text" value={imageUrl} onChange={handleInputChange} placeholder="Escribe la URL de la imagen aquí" />
+      <button onClick={handleAnalyzeButtonClick}>Analizar imagen</button>
+      <button onClick={handleGenerateButtonClick}>Generar imagen</button>
+    </div>
+  );
 }
 
 export default App;
